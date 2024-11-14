75acdc96d1a698e53904f97a408ce620a7f5df2e
annah11 <hana.mesfin@astu.edu.et>
2024-11-14 16:27:59 +0300

    for_else

---
+++ basic/for_else.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "mango","pineapple",
+"orange"]
+for fruit in fruits:
+    if fruit == "pineapple":
+        print("Found pineapple!")
+        continue
+    else:
+        print("Pineapple not found in the list.")
