d36d8b5541637cff44378ea9fc21eabcab7e1577
annah11 <hana.mesfin@astu.edu.et>
2024-11-17 15:37:22 +0300

    two pointer

--- basic/remove element.py
+++ basic/remove element.py
@@ -1,4 +1,9 @@
-r= 0
-for i in range(len(nums)):
-    if nums[i !=vla]
-    
+class solution
+    def removeElement(nums, val):
+        r= 0
+        for i in range(len(nums)):
+            if nums[i !=val]:
+                nums[r] = nums[i]
+                r += 1
+        return r
+            
