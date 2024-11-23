240eec2b308aa517549e16b9a51c733299f1926d
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2024-11-24 00:09:39 +0300

    Time: 5 ms (50.2%), Space: 16.7 MB (44.39%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,7 +3,7 @@ class Solution:
         for row in range(9):
             seen = set()
             for i in range(9):
-                if board[row][i] == ".": 
+                if board[row][i] == ".":
                     continue
                 if board[row][i] in seen:
                     return False
@@ -16,16 +16,16 @@ class Solution:
                 if board[i][col] in seen:
                     return False
                 seen.add(board[i][col])
-            
+
         for square in range(9):
             seen = set()
             for i in range(3):
                 for j in range(3):
-                    row = (square//3) * 3 + i
+                    row = (square // 3) * 3 + i
                     col = (square % 3) * 3 + j
                     if board[row][col] == ".":
                         continue
                     if board[row][col] in seen:
                         return False
                     seen.add(board[row][col])
-        return True    
+        return True
