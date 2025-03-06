d8d9ee9474b65d7d5f4d707afc4c8f876c430cad
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-03-06 17:45:30 +0300

    Time: 0 ms (100%), Space: 18.2 MB (21.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -5,6 +5,10 @@ class Solution:
         for num in reversed(nums2):
             while stack and stack[-1] <= num:
                 stack.pop()
-            next_greater[num] = stack[-1] if stack else -1
-            stack.append(num)        
-        return [next_greater[num] for num in nums1]
+            if stack:
+                next_greater[num] = stack[-1] 
+            else:
+                next_greater[num] = -1
+            stack.append(num)
+        return [next_greater[num] for num in nums1]     
+        
