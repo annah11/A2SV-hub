b772ea6b2878e950c8d4944572944804397515d7
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-03-26 16:02:16 +0300

    Time: 206 ms (34.49%), Space: 22.2 MB (54.66%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,12 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def canShip(capacity):
+            total, needed_days = 0, 1
+            for w in weights:
+                if total + w > capacity:
+                    needed_days += 1
+                    total = 0
+                total += w
+            return needed_days <= days
+
+        return bisect.bisect_left(range(max(weights), sum(weights) + 1), True, key=canShip) + max(weights)
