66c44aada2e5e6658ddb44e966418166cdf627f2
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-03 00:33:21 +0300

    Time: 189 ms (46.48%), Space: 32.9 MB (68.59%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -8,40 +8,33 @@ class Solution:
         if not head or not head.next:
             return head
 
-        mid = self.getMiddle(head)  
-        left = head  
-        right = mid.next 
-        mid.next = None  
-        left = self.sortList(left)
-        right = self.sortList(right)
+        mid = self.getMid(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
         return self.merge(left, right)
 
-    def getMiddle(self, head: ListNode) -> ListNode:
-        """
-        Finds the middle node using slow and fast pointers.
-        """
-        slow = head
-        fast = head
-        while fast.next and fast.next.next:
+    def getMid(self, head: ListNode) -> ListNode:
+        slow, fast = head, head.next
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return slow
-    def merge(self, list1: ListNode, list2: ListNode) -> ListNode:
-        """
-        Merges two sorted linked lists and returns the head of the merged list.
-        """
-        dummy = ListNode(0)  # Dummy node to simplify merging
+        mid = slow.next
+        slow.next = None  
+        return mid
+
+    def merge(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
         tail = dummy
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
             else:
-                tail.next = list2
-                list2 = list2.next
+                tail.next = l2
+                l2 = l2.next
             tail = tail.next
-
-        tail.next = list1 if list1 else list2
-
+        
+        tail.next = l1 if l1 else l2 
         return dummy.next
