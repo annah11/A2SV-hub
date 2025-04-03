f4b25e4e1b6b817944a58a22620d54cabe265e07
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-03 17:40:50 +0300

    Time: 46 ms (22.37%), Space: 26.9 MB (89.05%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -10,21 +10,21 @@ class Solution:
         return [nums[i] for i in range(1, len(nums)) if nums[i] == nums[i - 1]]
 
 
-    def quick_sort(self, nums: List[int], left: int, right: int):
-        if left >= right:
-            return
-        pivot_index = self.partition(nums, left, right)
-        self.quick_sort(nums, left, pivot_index - 1)
-        self.quick_sort(nums, pivot_index + 1, right)
+    # def quick_sort(self, nums: List[int], left: int, right: int):
+    #     if left >= right:
+    #         return
+    #     pivot_index = self.partition(nums, left, right)
+    #     self.quick_sort(nums, left, pivot_index - 1)
+    #     self.quick_sort(nums, pivot_index + 1, right)
     
 
-    def partition(self, nums: List[int], left: int, right: int) -> int:
-        pivot = nums[right]
-        i = left - 1
+    # def partition(self, nums: List[int], left: int, right: int) -> int:
+    #     pivot = nums[right]
+    #     i = left - 1
         
-        for j in range(left, right):
-            if nums[j] < pivot:
-                i += 1
-                nums[i], nums[j] = nums[j], nums[i]
-        nums[i + 1], nums[right] = nums[right], nums[i + 1]
-        return i + 1
+    #     for j in range(left, right):
+    #         if nums[j] < pivot:
+    #             i += 1
+    #             nums[i], nums[j] = nums[j], nums[i]
+    #     nums[i + 1], nums[right] = nums[right], nums[i + 1]
+    #     return i + 1
