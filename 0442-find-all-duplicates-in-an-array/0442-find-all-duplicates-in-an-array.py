d10784a023ff8d3bfd267b02311e1edc9750e761
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-03 18:19:56 +0300

    Time: 50 ms (13.9%), Space: 26.8 MB (95.08%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,30 +1,12 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        # self.quick_sort(nums, 0, len(nums) - 1)
-        # hinata = []
-        # for i in range(1,len(nums)):
-        #     if nums[i] == nums[i - 1]:
-        #         hinata.append(nums[i])        
-        # return hinata
-        nums.sort()
-        return [nums[i] for i in range(1, len(nums)) if nums[i] == nums[i - 1]]
+        n = len(nums)
+        i = 0
+        while i< n:
+            pos = nums[i] -1
+            if nums[pos] != nums[i]:
+                nums[pos], nums[i] = nums[i] , nums[pos]
+            else :
+                i +=1
+        return [nums[i] for i in range(len(nums)) if nums[i] != i + 1]
 
-
-    # def quick_sort(self, nums: List[int], left: int, right: int):
-    #     if left >= right:
-    #         return
-    #     pivot_index = self.partition(nums, left, right)
-    #     self.quick_sort(nums, left, pivot_index - 1)
-    #     self.quick_sort(nums, pivot_index + 1, right)
-    
-
-    # def partition(self, nums: List[int], left: int, right: int) -> int:
-    #     pivot = nums[right]
-    #     i = left - 1
-        
-    #     for j in range(left, right):
-    #         if nums[j] < pivot:
-    #             i += 1
-    #             nums[i], nums[j] = nums[j], nums[i]
-    #     nums[i + 1], nums[right] = nums[right], nums[i + 1]
-    #     return i + 1
