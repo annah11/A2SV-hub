5c57bdb55b341b7b052836e6ff723134f6fe1e58
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-09 17:50:21 +0300

    Time: 7 ms (55.4%), Space: 21.6 MB (68.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,34 +3,26 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # return 
-        if not board or not board[0]: return
-
-        rows, cols = len(board), len(board[0])
-        visited = set()
-
-        def dfs(r, c):
-            if (r < 0 or r >= rows or c < 0 or c >= cols or 
-                board[r][c] != 'O' or (r, c) in visited):
+        if not board or not board[0]:
+            return
+        rows,cols = len(board) ,len(board[0])
+        def dfs(r,c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
                 return
-            visited.add((r, c))
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-
-        def scan_border(row=0, col=0):
-            if row >= rows: return
-            if col >= cols:
-                scan_border(row + 1, 0)
-                return
-            if (row == 0 or row == rows - 1 or col == 0 or col == cols - 1):
-                dfs(row, col)
-            scan_border(row, col + 1)
-
-        scan_border()
-
+            board[r][c] = "S"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for r in range(rows):
+            dfs(r, 0)
+            dfs(r, cols - 1)
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows - 1, c)
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O' and (r, c) not in visited:
+                if board[r][c] == 'O':
                     board[r][c] = 'X'
+                elif board[r][c] == 'S':
+                    board[r][c] = 'O'
