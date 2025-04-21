f6c813c5355ff869d5404141df734bd6c6f9798d
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-21 09:05:42 +0300

    Time: 0 ms (100%), Space: 17.7 MB (97.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -4,13 +4,16 @@ class Solution:
         initally =image[sr][sc]
         if color == initally:
             return image
-        def dfs(i,j):
+        q = deque()
+        q.append((sr,sc))
+        while q:
+            i,j = q.popleft()
             if i <0 or i>=m or j<0 or j >= n or image[i][j] !=initally:
-                return 
+                continue
+             
             image[i][j] = color
-            dfs(i-1,j)
-            dfs(i+1,j)
-            dfs(i,j-1)
-            dfs(i,j+1)
-        dfs(sr,sc)
+            q.append((i-1,j))
+            q.append((i+1,j))
+            q.append((i,j-1))
+            q.append((i,j+1))
         return image
