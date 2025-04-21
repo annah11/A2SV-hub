4cc20fe991079495aa18d4fd5765661cc6e193f0
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-21 09:00:00 +0300

    Time: 0 ms (100%), Space: 18.2 MB (26.23%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m,n = len(image),len(image[0])
+        initally =image[sr][sc]
+        if color == initally:
+            return image
+        def dfs(i,j):
+            if i <0 or i>=m or j<0 or j >= n or image[i][j] !=initally:
+                return 
+            image[i][j] = color
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+        dfs(sr,sc)
+        return image
