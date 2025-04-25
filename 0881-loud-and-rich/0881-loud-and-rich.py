02125fc1dc5b2b5d7f91d15368692903a0be9188
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-25 18:09:06 +0300

    Time: 18 ms (44.71%), Space: 24.7 MB (24.62%) - LeetHub

--- 0881-loud-and-rich/0881-loud-and-rich.py
+++ 0881-loud-and-rich/0881-loud-and-rich.py
@@ -1,21 +1,20 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-        graph = defaultdict(list)
-        for u, v in richer:
-            graph[v].append(u)  
         n = len(quiet)
-        answer = [-1] * n
-
+        graph = defaultdict(list)
+        for u,v in richer:
+            graph[v].append(u)
+        print(graph)
+        res = [-1] *n
         def dfs(node):
-            if answer[node] != -1:
-                return answer[node]
-            answer[node] = node
-            for richer_person in graph[node]:
-                cand = dfs(richer_person)
-                if quiet[cand] < quiet[answer[node]]:
-                    answer[node] = cand
-            return answer[node]
+            if res[node] != -1:
+                return res[node]
+            res[node] = node
+            for ne in graph[node]:
+                cand = dfs(ne)
+                if quiet[cand] < quiet[res[node]]:
+                    res[node] = cand
+            return res[node]
         for i in range(n):
             dfs(i)
-
-        return answer
+        return res
