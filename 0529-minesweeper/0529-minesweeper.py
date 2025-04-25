cb625dcb9f8e3b6be58e07a398056c4c871e89b5
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-25 13:07:13 +0300

    Time: 10 ms (22.98%), Space: 20.5 MB (22.54%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.py
@@ -0,0 +1,29 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        rows, cols = len(board), len(board[0])
+        directions = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(-1,1),(1,-1),(1,1)]
+
+        def count_mines(r, c):
+            count = 0
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == 'M':
+                    count += 1
+            return count
+
+        def dfs(r, c):
+            if not (0 <= r < rows and 0 <= c < cols) or board[r][c] != 'E':
+                return
+            mines = count_mines(r, c)
+            if mines > 0:
+                board[r][c] = str(mines)
+            else:
+                board[r][c] = 'B'
+                for dr, dc in directions:
+                    dfs(r + dr, c + dc)
+        r, c = click
+        if board[r][c] == 'M':
+            board[r][c] = 'X'
+        else:
+            dfs(r, c)
+        return board
