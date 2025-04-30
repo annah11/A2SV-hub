0da9c76b168ccc754f9fedd8d9d5e74747cefbb5
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-04-30 16:38:15 +0300

    Time: 156 ms (41.79%), Space: 39.5 MB (55.38%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -13,6 +13,8 @@ class MedianFinder:
     def findMedian(self) -> float:
         if len(self.max_heap) > len(self.min_heap):
             return -self.max_heap[0]
+        if len(self.max_heap) < len(self.min_heap):
+            return -self.min[0] 
         return (-self.max_heap[0] + self.min_heap[0]) / 2
         
 
