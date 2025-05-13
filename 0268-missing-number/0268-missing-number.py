3842089856e58c6d4c66d62744c04523d87764bf
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-05-13 19:27:03 +0300

    Time: 0 ms (100%), Space: 18.7 MB (72.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,30 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-
-        self.quick_sort(nums, 0, len(nums) - 1)
-        
-        for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        
-        return len(nums)
-
-    def quick_sort(self, nums: List[int], left: int, right: int):
-        if left >= right:
-            return
-        
-        pivot_index = self.partition(nums, left, right)
-        self.quick_sort(nums, left, pivot_index - 1)
-        self.quick_sort(nums, pivot_index + 1, right)
-
-    def partition(self, nums: List[int], left: int, right: int) -> int:
-        pivot = nums[right]
-        i = left - 1
-        
-        for j in range(left, right):
-            if nums[j] < pivot:
-                i += 1
-                nums[i], nums[j] = nums[j], nums[i]
-        
-        nums[i + 1], nums[right] = nums[right], nums[i + 1]
-        return i + 1
+        n = len(nums)
+        return ((n *( n+1)) //2) - sum(nums)
