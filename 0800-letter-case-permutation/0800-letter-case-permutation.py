a8bd858d0c06e4775b7176280a8bf34f3ad6769a
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-05-24 13:29:55 +0300

    Time: 1 ms (90.86%), Space: 18.4 MB (92.28%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -1,17 +1,9 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        letter_indices = [i for i, c in enumerate(s) if c.isalpha()]
-        n = len(letter_indices)
-        res = []
-
-        for mask in range(1 << n):  # 2^n combinations
-            chars = list(s)
-            for i in range(n):
-                idx = letter_indices[i]
-                if (mask >> i) & 1:
-                    chars[idx] = chars[idx].upper()
-                else:
-                    chars[idx] = chars[idx].lower()
-            res.append("".join(chars))
-        
-        return res
+        result = [""]
+        for ch in s:
+            if ch.isalpha():
+                result = [res + c for res in result for c in [ch.lower(), ch.upper()]]
+            else:
+                result = [res + ch for res in result]
+        return result
