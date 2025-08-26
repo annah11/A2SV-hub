7d7ab558f354fee2053fa1cc9286bb2b0929c4b2
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-08-26 21:30:51 +0300

    Time: 19 ms (50.45%), Space: 16.1 MB (12.5%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,27 +1,28 @@
-class Solution:
-    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-
+class Solution(object):
+    def findDiagonalOrder(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[int]
+        """
         if not mat or not mat[0]:
             return []
-        
+
         m, n = len(mat), len(mat[0])
         result = []
         diagonals = {}
 
-    
         for i in range(m):
             for j in range(n):
                 if i + j not in diagonals:
                     diagonals[i + j] = []
                 diagonals[i + j].append(mat[i][j])
-        
-    
+
         for key in range(m + n - 1):
             if key % 2 == 0:
-            
+
                 result.extend(diagonals[key][::-1])
             else:
-            
+
                 result.extend(diagonals[key])
-        
+
         return result
