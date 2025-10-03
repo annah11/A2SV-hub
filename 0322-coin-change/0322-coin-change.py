26aee7acbd3b1880d9d12e8adf21aca4db969d8b
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-10-03 12:23:11 +0300

    Time: 706 ms (80.33%), Space: 18.2 MB (68.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
-        def helper(coins,amount):
-            if amount in memo:
-                return memo[amount] 
-            if amount == 0:
-                return 0
-            if amount < 0:
-                return float('inf')
-            min_coins = float('inf')
-            for coin in coins:
-                res = helper(coins, amount - coin)
-                if res != float('inf'):
-                    min_coins = min(min_coins, res + 1)
-            memo[amount] = min_coins
-            return min_coins
-        result =  helper(coins,amount)
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0 
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
 
-        if result == float('inf'):
-            return -1
-        return result
+        return dp[amount] if dp[amount] != float("inf") else -1
+        
+        # dp = [[0] * (amount+1) for _ in range(len(coins)+1)]
+        # for i in range(len(coins)+1):
+        #     dp[i][0] =1
+        # for i in range(1,len(coins)+1):
+        #     for j in range(1,amount+1):
+        #         if coins[i-1] >j:
+        #             dp[i][j]=dp[i-1][j]
+        #         else:
+        #             dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+        # return dp[len(coins)][amount]
