7db97fcf9be207b313af95bb4e442cc574b42c7b
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-10-17 11:59:24 +0300

    Time: 3 ms (97.62%), Space: 18.2 MB (59.69%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,47 +1,11 @@
-class TrieNode:
-    def __init__(self):
-        self.is_end = False
-        self.children = [None for _ in range(26)]
 class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-    
-
-    def insert(self, word):
-        # if len(word) == 1:
-        #     return True
-        cur = self.root
-        for i, char in enumerate(word):
-            idx = ord(char) - ord('a')
-            if not cur.children[idx]:
-                cur.children[idx] = TrieNode()
-            cur = cur.children[idx]
-        cur.is_end = True
-        return True
-
-    def can_build(self, word):
-        cur = self.root
-        for char in word:
-            idx = ord(char) - ord('a')
-            cur = cur.children[idx]
-            if not cur or not cur.is_end:
-                return False
-        return True
-    
     def longestWord(self, words: List[str]) -> str:
-        for word in words:
-            self.insert(word)
-
         words.sort()
-
-        longest = ""
+        mp = set([""])
+        res = ""
         for word in words:
-            if self.can_build(word):
-                if len(longest) < len(word):
-                    longest = word
-        return longest
-
-
-            
-
-        
+            if word[:-1] in mp:
+                mp.add(word)
+                if len(word) > len(res) or (len(word) == len(res) and word < res):
+                    res = word
+        return res
