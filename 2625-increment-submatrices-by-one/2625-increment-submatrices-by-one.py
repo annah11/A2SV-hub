58d38019a8080df0f1eda10f661979b234eeeed5
Hana Mesfin <137293765+annah11@users.noreply.github.com>
2025-11-14 09:51:12 +0300

    Time: 287 ms (39.9%), Space: 39.2 MB (55.4%) - LeetHub

---
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        mat = [[0] * n for _ in range(n)]
+        print(mat)
+        for r1,c1,r2,c2 in queries:
+            mat[r1][c1]+=1
+            if r2+1< n:
+                mat[r2+1][c1] -=1
+            if c2+1<n:
+                mat[r1][c2+1] -=1
+            if r2+1 < n and c2+1<n:
+                mat[r2+1][c2+1] +=1
+        for i in range(n):
+            for j in range(n):
+                if i >0:
+                    mat[i][j] += mat[i-1][j]
+                if j > 0:
+                    mat[i][j] += mat[i][j-1]
+                if i>0 and j>0:
+                    mat[i][j] -= mat[i-1][j-1]
+        return mat
